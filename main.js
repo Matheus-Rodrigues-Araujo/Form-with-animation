fa47d19019d8b3ab4440d0b579bc02d9aac6b5c4
Matheus-Rodrigues-Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2022-05-15 15:18:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const submitBtn = document.getElementById("submit-btn")
+const info = document.getElementById("info")
+
